6ea3ca3abcde6c1b3e0673430037fadaa3eeea2c
abhishek singh <abhisheksingh@Abhisheks-MacBook-Air.local>
2024-01-03 01:16:21 +0530

    dd

---
+++ src/week_01/Java_code_01/Binary.java
@@ -0,0 +1,10 @@
+package week_01.Java_code_01;
+
+import java.sql.SQLOutput;
+
+public class Binary {
+    public static void main(String[] args) {
+        System.out.println("Binary Number :");
+
+    }
+}
